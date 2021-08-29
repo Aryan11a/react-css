e0b211857285c3452b9d494bb092ee2baab1428a
Aryan11a <aryanrana123y@gmail.com>
2021-08-29 11:34:43 +0545

    my java

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+
+
+//main fn()
+export default function index () {
+//your variable, function and hooks go here
+
+//main return
+  return (
+    <>
+      <div className="text-red-600 m-6 p-5 bg-green-300 text-3xl shadow-xl rounded-b-lg">Hello from index</div>
+    </>
+  )
+};
